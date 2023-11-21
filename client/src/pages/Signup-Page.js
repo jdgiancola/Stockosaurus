6ff730ceb55e7c99d37d13f4a8446f393ca39faf
Jo Giancola <josephdgiancola@gmail.com>
2023-11-20 19:56:43 -0600

    simple layout for react Signup-Page

---
+++ client/src/pages/Signup-Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignupPage = () => {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      {/* Form for signing up */}
+    </div>
+  );
+};
+
+export default SignupPage;
