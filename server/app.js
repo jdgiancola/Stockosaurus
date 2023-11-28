a56ede727b064939365d08a96966a0b26dd4ccc0
amattock <134568545+amattock@users.noreply.github.com>
2023-11-27 21:57:40 -0600

    build

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,7 +6,7 @@ const path = require('path');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema'); // Assuming your GraphQL schema is here
 
-
+require('dotenv').config();
 
 // Express app initialization
 const app = express();
