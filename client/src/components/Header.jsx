0895e229c4aa47425186b02411a19538245d5529
amattock <134568545+amattock@users.noreply.github.com>
2023-11-28 19:51:02 -0600

    added to donation.jsx/ fixed spelling in headers

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
           </h1>
         </Navbar.Brand>
         <Navbar.Text className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Your cool new iventory system!
+          Your cool new inventory system!
         </Navbar.Text>
         <Nav>
           {Auth.loggedIn() ? (
--- client/src/pages/Donate.jsx
+++ client/src/pages/Donate.jsx
@@ -1,29 +1,62 @@
-import React from 'react';
-import '../Styles/LoginPage.css'; 
+import React, { useState } from 'react';
+import { CardElement, useStripe, useElements } from 'react-stripe-elements';
+import '../Styles/Donate.css';
 
-const DonatePage = () => {
+const DonateForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleDonate = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Create a token or handle card details using elements
+    const { token, error } = await stripe.createToken(elements.getElement(CardElement), {
+      name: name,
+      email: email,
+    });
+
+    if (error) {
+      console.error(error);
+    } else {
+      // Handle the token (send to server, update UI, etc.)
+      console.log(token);
+    }
+  };
+
   return (
-    <div className="Donate-container">
-      <div className="Donate-header">
-        <h1>Donate to the STOCKASAUROUSES</h1>
-      </div>
-      <div className="Donate-form">
-        <h2>Create an account</h2>
-        <p>Let's get started!</p>
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="amount" placeholder="Amount" />
-          <button type="submit">Donate!</button>
-        </form>
-        <div className="Donate-footer"> 
-        </div>
-      </div>
-      <div className="login-illustration">
-        {/* Place your image or illustration here */}
-      </div>
-    </div>
+    <form onSubmit={handleDonate}>
+      <label>
+        Name
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Email
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Amount
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </label>
+      <label>
+        Card Details
+        <CardElement />
+      </label>
+      <button type="submit" disabled={!stripe}>
+        Donate!
+      </button>
+    </form>
   );
 };
 
-export default DonatePage;
+export default DonateForm;
+
+
