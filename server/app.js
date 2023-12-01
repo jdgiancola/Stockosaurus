b54b03ff2c90b7eea7358e15bd258b219e4a6e7b
Jo Giancola <josephdgiancola@gmail.com>
2023-11-30 20:20:05 -0600

    app.js and package.json

--- server/app.js
+++ server/app.js
@@ -5,15 +5,29 @@ const multer = require('multer');
 const path = require('path');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema'); // Assuming your GraphQL schema is here
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 require('dotenv').config();
 
 // Express app initialization
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+// Apollo Server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  app.use('/graphql', expressMiddleware(server));
+};
+
+startApolloServer();
+
 
 // Middleware
 app.use(cors());
@@ -63,16 +77,18 @@ app.post('/upload', (req, res) => {
   });
 });
 
+// Static file serving for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
-
-// GraphQL server setup
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
 
 // Server listening
 const PORT = process.env.PORT || 5001;
